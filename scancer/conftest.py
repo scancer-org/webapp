e03ce28636bfcf9c058c1ceae12e486e5538b65f
Harish Narayanan <hnarayanan@gmail.com>
2021-04-06 17:45:47 +0100

    Create initial project

---
+++ scancer/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from scancer.users.models import User
+from scancer.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
