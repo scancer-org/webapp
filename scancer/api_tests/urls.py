250bad2bda520c3bfe1b9667d628b2f1cc2e0aed
Harish Narayanan <hnarayanan@gmail.com>
2021-04-12 14:27:14 +0000

    Add a new view to test an image segmenter API

--- scancer/api_tests/urls.py
+++ scancer/api_tests/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import MNISTImageClassifierView
+from .views import MNISTImageClassifierView, DeepLabv3ImageSegmenterView
 
 app_name = "api_tests"
 urlpatterns = [
     path("mnist/", view=MNISTImageClassifierView.as_view(), name="mnist"),
+    path("deeplabv3/", view=DeepLabv3ImageSegmenterView.as_view(), name="deeplabv3"),
 ]
--- scancer/api_tests/views.py
+++ scancer/api_tests/views.py
@@ -2,14 +2,20 @@ import requests
 from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import FormView
+from django.urls import reverse_lazy
 
 from .forms import ImageUploadForm
 
 
 class MNISTImageClassifierView(LoginRequiredMixin, FormView):
-    template_name = "api_tests/mnist.html"
+    template_name = "api_tests/image_upload.html"
     form_class = ImageUploadForm
-    success_url = "/api-tests/mnist"
+    success_url = reverse_lazy("api_tests:mnist")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Test of the MNIST Digit Classifier API"
+        return context
 
     def form_valid(self, form):
         r = requests.put(
@@ -20,5 +26,28 @@ class MNISTImageClassifierView(LoginRequiredMixin, FormView):
             self.request,
             messages.INFO,
             f"The file you uploaded was the number {number}",
+        )
+        return super().form_valid(form)
+
+
+class DeepLabv3ImageSegmenterView(LoginRequiredMixin, FormView):
+    template_name = "api_tests/image_upload.html"
+    form_class = ImageUploadForm
+    success_url = reverse_lazy("api_tests:deeplabv3")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "Test of the DeepLabv3 (ResNet 101 backbone) Image Segmenter API"
+        return context
+
+    def form_valid(self, form):
+        r = requests.put(
+            "http://localhost:8080/predictions/deeplabv3_resnet_101", data=form.cleaned_data["image"]
+        )
+        segments = r.text
+        messages.add_message(
+            self.request,
+            messages.INFO,
+            f"The file you uploaded has the following segments {segments}",
         )
         return super().form_valid(form)
