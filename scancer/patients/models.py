aed966178423148547dde62e4364c20e16a06778
Harish Narayanan <hnarayanan@gmail.com>
2021-04-28 23:22:25 +0100

    Make the list of patients data driven

--- scancer/patients/models.py
+++ scancer/patients/models.py
@@ -1,4 +1,6 @@
+from dateutil.relativedelta import relativedelta
 from django.db import models
+from django.utils import timezone
 
 from .choices import SEX_CHOICES
 
@@ -13,3 +15,10 @@ class Patient(models.Model):
 
     def __str__(self):
         return self.name
+
+    @property
+    def age(self):
+        "Calculate the age of a patient from their date of birth"
+        now = timezone.now().date()
+        age = relativedelta(now, self.date_of_birth)
+        return age
--- scancer/patients/views.py
+++ scancer/patients/views.py
@@ -1,7 +1,11 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.views.generic import ListView
+
+from .models import Patient
 
 
-class PatientListView(LoginRequiredMixin, TemplateView):
+class PatientListView(LoginRequiredMixin, ListView):
 
+    model = Patient
+    context_object_name = "patients"
     template_name = "patients/list.html"
