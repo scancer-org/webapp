93630ab9234ace4c11e39dad9ed805c57d9d4f72
Harish Narayanan <hnarayanan@gmail.com>
2021-05-11 12:22:33 +0100

    Adjust the descriptive strings on the backend

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,10 @@ from django.views import defaults as default_views
 from django.views.generic import TemplateView
 from rest_framework.authtoken.views import obtain_auth_token
 
+admin.site_tile = "Scancer Backend"
+admin.site.site_header = "Scancer Administration"
+admin.index_title = "Scancer Administration"
+
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path(
