c68489e22d0c88e691aee32bfe088014e006fd6c
Harish Narayanan <hnarayanan@gmail.com>
2021-05-11 14:24:24 +0100

    Add a Scan model

--- scancer/examinations/admin.py
+++ scancer/examinations/admin.py
@@ -1,9 +1,18 @@
 from django.contrib import admin
 
-from .models import Examination
+from .models import Examination, Scan
+
+
+class ScanInline(admin.StackedInline):
+    model = Scan
+    extra = 1
+    max_num = 10
 
 
 class ExaminationAdmin(admin.ModelAdmin):
+    inlines = [
+        ScanInline,
+    ]
     list_display = ("kind", "date", "patient", "healthcare_professional", "priority")
     list_filter = ("priority",)
 
--- scancer/examinations/models.py
+++ scancer/examinations/models.py
@@ -1,9 +1,16 @@
+import os
+
+from django.conf import settings
 from django.db import models
 from django.urls import reverse
 
 from scancer.patients.models import Patient
 
 from .choices import KIND_CHOICES, PRIORITY_CHOICES
+
+
+def scan_images_path():
+    return os.path.join(settings.APPS_DIR, "examples")
 
 
 class Examination(models.Model):
@@ -26,3 +33,13 @@ class Examination(models.Model):
         "Get a Bootstrap colour name associated with the examination priority"
         colour_map = {"high": "danger", "medium": "warning", "low": "success"}
         return colour_map.get(self.priority, "secondary")
+
+
+class Scan(models.Model):
+
+    examination = models.ForeignKey(Examination, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    file = models.FilePathField(path=scan_images_path)
+
+    def __str__(self):
+        return f"{self.file} ({self.date})"
